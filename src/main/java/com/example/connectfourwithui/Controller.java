66f8a3da004956d95e158e4619b0f25f0507cdc7
p20100 <71640994+Lilly05@users.noreply.github.com>
2022-10-14 14:33:32 +0200

    every spot can now be accessed

--- src/main/java/com/example/connectfourwithui/Controller.java
+++ src/main/java/com/example/connectfourwithui/Controller.java
@@ -19,28 +19,75 @@ public class Controller {
         FontAwesomeIcon icon = new FontAwesomeIcon();
         icon.setGlyphName("APPLE");
         icon.setSize("2em");
-        /*grid.getChildren().removeIf(node -> (!(GridPane.getRowIndex(node) == null) && GridPane.getRowIndex(node) == 4) && (GridPane.getColumnIndex(node) == null || GridPane.getColumnIndex(node) == 0));
-        grid.add(icon, 0, 4);*/
         for (int row = 4; row >= 0; row--){
-            if(isIconRemoved(row, null, grid, icon)){
+            if(row == 0) {
+                grid.getChildren().removeIf(node -> (GridPane.getRowIndex(node) == null || GridPane.getRowIndex(node) == 0) && (GridPane.getColumnIndex(node) == null || GridPane.getColumnIndex(node) == 0));
+                grid.add(icon, 0, 0);
+                break;
+            }else if (isIconRemoved(row, null, grid)){
                 grid.add(icon, 0, row);
                 break;
             }
         }
         }
 
+    @FXML
+    protected void onButtonTwo(){
+        setIcon(1, "APPLE");
+    }
 
-    public boolean isIconRemoved(Integer row, Integer column, GridPane grid, FontAwesomeIcon icon) {
+    @FXML
+    protected void onButtonThree(){
+        setIcon(2, "APPLE");
+    }
+
+    @FXML
+    protected void onButtonFour(){
+        setIcon(3, "APPLE");
+    }
+
+    @FXML
+    protected void onButtonFive(){
+        setIcon(4, "APPLE");
+    }
+
+    @FXML
+    protected void onButtonSix(){
+        setIcon(5, "APPLE");
+    }
+
+    @FXML
+    protected void onButtonSeven(){
+        setIcon(6, "APPLE");
+    }
+
+    public void setIcon(Integer column, String iconName){
+        FontAwesomeIcon icon = new FontAwesomeIcon();
+        icon.setGlyphName(iconName);
+        icon.setSize("2em");
+        for (int row = 4; row >= 0; row--){
+            if(isIconRemoved(row, column, grid)){
+                grid.add(icon, column, row);
+                break;
+            }
+        }
+    }
+
+
+    public boolean isIconRemoved(Integer row, Integer column, GridPane grid) {
         ObservableList<Node> childrens = grid.getChildren();
 
-        if(row == 0){
-            grid.getChildren().removeIf(node -> (GridPane.getRowIndex(node) == null || GridPane.getRowIndex(node) == 0) && (GridPane.getColumnIndex(node) == null || GridPane.getColumnIndex(node) == 0));
-            return true;
-        }
         for (Node child : childrens) {
-            if(GridPane.getRowIndex(child) == row && GridPane.getColumnIndex(child) == column) {
-                grid.getChildren().removeIf(node -> (!(GridPane.getRowIndex(node) == null) && GridPane.getRowIndex(node) == row) && (GridPane.getColumnIndex(node) == null || GridPane.getColumnIndex(node) == 0));
+            if(row == 0) {
+                grid.getChildren().removeIf(node -> (GridPane.getRowIndex(node) == null || GridPane.getRowIndex(node) == row) && (GridPane.getColumnIndex(node) == column));
                 return true;
+            }
+            if(GridPane.getRowIndex(child) == row && GridPane.getColumnIndex(child) == column) {
+                FontAwesomeIcon parsedIcon = (FontAwesomeIcon) child;
+                if(parsedIcon.getGlyphName().equals("CIRCLE_THIN")) {
+                    grid.getChildren().removeIf(node -> (GridPane.getRowIndex(node) == row && GridPane.getColumnIndex(node) == column));
+                    return true;
+                }
             }
         }
         return false;
